a7eaf97a911b4b4be2dfbe12808e2b70bef7d3cf
Gorav-K <78222737+Gorav-K@users.noreply.github.com>
2021-12-22 02:19:28 -0500

    feature add get the time for each station on the path

--- main.js
+++ main.js
@@ -6,44 +6,16 @@ let pathURL = "http://10.101.0.12:8080/path/"
 // SCHEDULE AT STATION: http://10.101.0.12:8080/schedule/Sainte-Dorothée //all passage times by segment 
  let scheduleAtStationURL = "http://10.101.0.12:8080/schedule/" 
 
-// DISTANCE BETWEEN TWO STATIONS: http://10.101.0.12:8080/distance/Sainte-Dorothée/Bois-Franc //distance in km between first station name (Sainte-Dorothée) to the second station name (Bois-Franc)
-// TRAIN SPEED: http://10.101.0.12:8080/averageTrainSpeed // the average speed in km/hr for all trains in the network
+let DistanceBtw2Stations = "http://10.101.0.12:8080/distance/" //distance in km between first station name (Sainte-Dorothée) to the second station name (Bois-Franc)
+let trainSpeedURL = "http://10.101.0.12:8080/averageTrainSpeed" // the average speed in km/hr for all trains in the network
 
 // Not necessary for the project, but in case you are interested, this endpoint gives you all the segments in the network: http://10.101.0.12:8080/segments
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let sStation =document.getElementById("start-station")
 let eStation =document.getElementById("end-station")
-let startTime =document.getElementById("startTime")
+let userStartTime =document.getElementById("startTime")
 let date = document.getElementById("date")
-
-
-
 
 
 //this populate drop down menu
@@ -82,16 +54,82 @@ async function GetPath(data){
 
     time = await ReturnFetchResponse(scheduleStationURL)
 
-    let reformatTime = time.filter(function (time){ if( time.SegmentId === data[0].SegmentId) 
+    let reformatTime = time.filter(function (time){ if( time.SegmentId === data[0].SegmentId) // this remove extra bit in front of the time and after the time and it filter time
         {time.Time = time.Time.split("T").pop()
 
         time.Time = time.Time.split(".").reverse().pop()
 
-        if( time.Time >= startTime.value){
+        if( time.Time >= userStartTime.value){
             return time
         }}});
 
-    console.log(reformatTime);
+    //console.log(reformatTime);
+
+    let speed = await GetSpeed()
+    let startingTime = reformatTime[0].Time;
+
+    console.log(data[0].Name +"Stating time: "+ startingTime)
+    let nextTime = startingTime
+    for (let i = 1; i < data.length; i++){
+        let distance =await GetDistance(data[i-1].Name,data[i].Name)
+        nextTime = GetTime(nextTime,distance,speed)
+        console.log(nextTime);
+    }
+    
+} 
+
+async function GetSpeed(){
+    
+    let speedAVG = await ReturnFetchResponse(trainSpeedURL)
+    console.log(speedAVG);
+    return speedAVG[0].AverageSpeed
+}
+
+async function GetDistance(station1,station2){
+        let DistanceBtw2StationsURL = DistanceBtw2Stations+encodeURIComponent(station1)+"/"+encodeURIComponent(station2)
+        return await ReturnFetchResponse(DistanceBtw2StationsURL)
+}
+async function GetTime(originalTime,distance,speed){
+
+    let time= distance/speed // km/(km/h)= ...hours
+    time= time*3600;
+    let hours= Math.floor(time / 3600)
+    let minutes = Math.floor(( time - (hours * 3600)) / 60); 
+    let seconds =Math.floor(time - (hours * 3600) - (minutes * 60));
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+
+   // let x = originalTime.split(":")
+/// need to fix this
+    // if( 60 >= (x[2] +seconds) ){
+    //     x[1]=x[1]+1
+    //     x[2]= (x[2] +seconds)-60
+    // }
+    // else{
+    //     x[2]= x[2] +seconds
+    // }
+    // if( 60 >= x[1] +minutes ){
+    //     x[0]=x[0]+1
+    //     x[1]= (x[1] +minutes)-60
+    // }
+    // else{
+    //     x[1]= x[1] +minutes
+    // }
+    // if( 24 >= x[0] + hours ){
+
+    //     x[0] = 00
+    //     x[0]= x[0]+hours;
+    // }
+    // else{
+    //     x[0]= x[0] + minutes
+    // }
+
+    let newtime = hours+":"+minutes+":"+seconds
+    console.log(newtime)
+    return newtime
+    
+
 }
 
 
@@ -249,7 +287,7 @@ function getinfo(data){
 
 // async function StationPath (data,id)
 // {
-//     console.log(startTime.value)
+//     console.log(userStartTime.value)
 //     console.log(data)
 //     scheduleAtStationURL=scheduleAtStationURL+encodeURIComponent(sStation.value)
 //     let time = await ReturnFetchResponse(scheduleAtStationURL)
